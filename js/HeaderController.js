b8cd6f0ea28a231c5eab81e1ffe232e27f4d3054
Henrique Albuquerque <henrique_barretto@outlook.com>
2019-10-29 13:25:38 -0300

    lul

---
+++ js/HeaderController.js
@@ -0,0 +1,45 @@
+const header = document.getElementById('header');
+const headerUlLi = document.querySelector('#header ul li');
+const headerUlList = document.querySelectorAll('#header ul li');
+const logo = document.getElementById('logo');
+
+const firstScreen = window.innerHeight;
+const secondScreen = window.innerHeight + document.getElementById('sobre-flex').height;
+
+window.onscroll = () => {
+
+    if (window.pageYOffset > 160) {
+        header.style.background = 'rgba(0, 0, 0, 0.20)';
+        header.style.height = '5rem';
+
+        logo.style.fontSize = '1.5rem';
+        logo.style.padding = '1rem 1rem';
+
+        headerUlLi.style.fontSize = '1rem';
+    } else {
+        header.style.background = 'none';
+        header.style.height = '7rem';
+
+        logo.style.fontSize = '2rem';
+        logo.style.padding = '1rem 1rem';
+
+        headerUlLi.style.fontSize = '1.2rem';
+    }
+
+    if (window.pageYOffset > (firstScreen - 50) && window.pageYOffset < (firstScreen + firstScreen)) {
+        headerUlList[0].classList.remove('aba-selected');
+        headerUlList[1].className = 'aba-selected';
+        headerUlList[2].classList.remove('aba-selected');
+    } else if (window.pageYOffset > (firstScreen + firstScreen)) {
+        headerUlList[1].classList.remove('aba-selected');
+        headerUlList[0].classList.remove('aba-selected');
+        headerUlList[2].className = 'aba-selected';
+    } else {
+        headerUlList[1].classList.remove('aba-selected');
+        headerUlList[0].className = 'aba-selected';
+        headerUlList[2].classList.remove('aba-selected');
+    }
+
+
+
+}
