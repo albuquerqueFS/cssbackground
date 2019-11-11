4430823aa0892517fb57bbc3d98233019c26ae8a
Henrique Albuquerque <henrique_barretto@outlook.com>
2019-11-11 00:46:10 -0300

    s

---
+++ js/InputVerify.js
@@ -0,0 +1,5 @@
+const inputNome = document.getElementById('nome');
+const inputEmail = document.getElementById('email');
+const inputMensagem = document.getElementById('mensagem');
+const enviarEmail = document.getElementById('enviarEmail');
+
